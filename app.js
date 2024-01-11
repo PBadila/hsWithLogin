faedb61f2441d25eec84ebeb9da0bb576d2a8cb7
PBadila <ritualmomma@gmail.com>
2024-01-10 22:20:31 -0500

    Adds cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use(
     session({
